cef790d36672664ebff3a9ad0ff257ac2a663c8f
Nan Wu <virus00719@gmail.com>
2018-10-12 19:29:26 -0400

    Update userName.js

--- public/js/login_js/userName.js
+++ public/js/login_js/userName.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
     $.get("/api/users").then(function(obj) {
       $(".userName").text(obj.body.firstname + " " + obj.body.lastname);
     });
