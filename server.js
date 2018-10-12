4b958c0d2fdd4edad3b38fcae0ea1b9d00d0f511
Nan Wu <virus00719@gmail.com>
2018-10-12 19:22:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,36 @@ const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
+var mysql      = require("mysql");
+var db_config = {
+  host     : "us-cdbr-iron-east-01.cleardb.net", 
+  user     : "b02bc8980bfbea",
+  password : "7119316b",
+  database : "heroku_7b1914202d5b895"
+};
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {              
+    if(err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }                                     
+  });                                     
+                                          
+  connection.on("error", function(err) {
+    console.log("db error", err);
+    if(err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();                         
+    } else {
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
+
 // Middleware
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
